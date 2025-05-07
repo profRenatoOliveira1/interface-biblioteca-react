07e48bdb5427192c6cf842634e305c2e6e87f623
Renato Oliveira <renato.loliveira89@gmail.com>
2025-05-07 11:49:02 -0300

    Criando formulário aluno

---
+++ src/components/Formularios/FormAluno/FormAluno.tsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+
+function FormAluno() {
+    const [formData, setFormData] = useState({
+        nome: '',
+        sobrenome: '',
+        dataNascimento: '',
+        endereco: '',
+        email: '',
+        celular: ''
+    });
+
+    // Função para atualizar o state
+    const handleChange = (nome: string, valor: string) => {
+        setFormData({ ...formData, [nome]: valor });
+    };
+
+    return (
+        <section>
+            <form action="" onSubmit={() => alert(JSON.stringify(formData))}>
+                <label htmlFor="">
+                    Nome
+                    <input 
+                        type="text" 
+                        name="nome" 
+                        id="nome"
+                        required
+                        minLength={3}
+                        onChange={(e) => handleChange("nome", e.target.value)}
+                    />
+                </label>
+                <label htmlFor="">
+                    Sobrenome
+                    <input 
+                        type="text" 
+                        name="sobrenome" 
+                        id="sobrenome"
+                        required
+                        minLength={3}
+                        onChange={(e) => handleChange("sobrenome", e.target.value)}
+                    />
+                </label>
+                <label htmlFor="">
+                    Data de Nascimento
+                    <input 
+                        type="date" 
+                        name="dataNascimento" 
+                        id="dataNascimento"
+                        onChange={(e) => handleChange("dataNascimento", e.target.value)}
+                    />
+                </label>
+                <label htmlFor="">
+                    Endereço
+                    <input 
+                        type="text" 
+                        name="endereco" 
+                        id="endereco"
+                        minLength={6}
+                        onChange={(e) => handleChange("endereco", e.target.value)}
+                    />
+                </label>
+                <label htmlFor="">
+                    E-mail
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email"
+                        minLength={11}
+                        onChange={(e) => handleChange("email", e.target.value)}
+                    />
+                </label>
+                <label htmlFor="">
+                    Celular
+                    <input 
+                        type="number" 
+                        name="celular" 
+                        id="celular"
+                        minLength={10}
+                        maxLength={13}
+                        onChange={(e) => handleChange("celular", e.target.value)}
+                    />
+                </label>
+
+                <input type="submit" value="CADASTRAR ALUNO" />
+            </form>
+        </section>
+    );
+}
+
+export default FormAluno;
